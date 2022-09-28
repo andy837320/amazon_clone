9017b10a05ef5a185f1c2c061c150f93cf8fca9a
andy837320 <114247046+andy837320@users.noreply.github.com>
2022-09-28 16:33:19 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Header from "./Header.js";
+import Home from "./Home.js";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout.js";
+import Login from "./Login.js";
+import { auth } from "./firebase.js";
+import { useStateValue } from "./StateProvider";
+import Payment from "./Payment.js";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./Orders.js";
+
+const promise = loadStripe(
+  "pk_test_51K8DHdDx0rjVHnEqGbaXFrz7AULZJmmY4CRO6NRGkej3GvzvdsLQjn4wZ0a3pCVtcD2JlagjuqYJjd1uDfZrT1vR00eaoKd80I"
+);
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  //useEffect actually like IF statemaent in react
+
+  useEffect(() => {
+    //it only run when app component loads..
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
+
+      if (authUser) {
+        //the user just logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/Checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
